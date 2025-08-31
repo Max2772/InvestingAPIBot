28308759aa73a16def00f8c8d3b7a98d2887bd99
Bibikau Maxim <bib.maxim@gmail.com>
2025-08-31 23:14:49 +0300

    Added -p parameter for /add commands

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
 from src.logger import get_logger, setup_logger
 from src.utils import profit_emoji, profit_sign, get_api_url, fetch_api_data
+from src.re_patterns import science_price_pattern, integer_price_pattern, float_price_pattern
---
+++ src/re_patterns.py
@@ -0,0 +1,3 @@
+integer_price_pattern = r"(\d+)"
+float_price_pattern = r"(\d+(\.\d+)?)"
+science_price_pattern = r"(\d+(\.\d+)?|\d+(\.\d+)?[eE][+-]\d+)"
