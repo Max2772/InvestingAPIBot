72f4e1d646c408f6295acad85eb75306a73901da
Bibikau Maxim <bib.maxim@gmail.com>
2025-09-01 20:21:11 +0300

    Changed name to fit for redis container

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 API_BASE_URL = "http://127.0.0.1:8000"
-REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379")
+REDIS_URL = os.getenv("REDIS_URL", "redis://investapi_redis:6379")
 THROTTLE_FIRST_LIMIT = 2
 THROTTLE_SECOND_LIMIT = 5
 MAXIMUM_ALERTS = 10
