4209ea8b1051f09a99111a56c1f203394a5dcdf3
Bibikau Maxim <bib.maxim@gmail.com>
2025-09-01 19:58:35 +0300

    Changed localhost to 6379 for redis

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 API_BASE_URL = "http://127.0.0.1:8000"
-REDIS_URL = "redis://localhost:6379"
+REDIS_URL = "redis://6379:6379"
 THROTTLE_FIRST_LIMIT = 1
 THROTTLE_SECOND_LIMIT = 5
 MAXIMUM_ALERTS = 10
