6320dabcc1d66dc348035e1d3055d7fde427c7e2
Bibikau Maxim <bib.maxim@gmail.com>
2025-09-01 20:19:15 +0300

    Support for .env for redis

--- src/config.py
+++ src/config.py
@@ -3,10 +3,11 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
+
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 API_BASE_URL = "http://127.0.0.1:8000"
-REDIS_URL = "redis://redis:6379"
-THROTTLE_FIRST_LIMIT = 1
+REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379")
+THROTTLE_FIRST_LIMIT = 2
 THROTTLE_SECOND_LIMIT = 5
 MAXIMUM_ALERTS = 10
-ALERT_INTERVAL_SECONDS = 300
+ALERT_INTERVAL_SECONDS = 10
